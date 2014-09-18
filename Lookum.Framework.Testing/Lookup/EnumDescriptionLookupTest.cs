a76c276951b4202892831ca88231bb5a710d1fe0
Cedric L. Charlier <cedric.charlier@yucom.be>
2014-09-18 14:49:56 +0200

    A bit of cleanup

---
+++ Lookum.Framework.Testing/Lookup/EnumDescriptionLookupTest.cs
@@ -0,0 +1,43 @@
+﻿using Lookum.Framework.Lookup;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+
+
+namespace Lookum.Framework.Testing.Lookup
+{
+    public class EnumDescritpionLookupTest
+    {
+        
+        public enum Status : short
+        {
+            [System.ComponentModel.Description("Currently waiting that you do something")]
+            Waiting = 0,
+            [System.ComponentModel.Description("Currently in action")]
+            InAction = 1
+        }
+
+        public class StatusLookup : EnumDescriptionLookup<Status, short>
+        {
+            public StatusLookup()
+                : base("Unknown")
+            {
+
+            }
+        }
+
+        [Test]
+        public void Match_AdvancedExistingKey_ReturnValue()
+        {
+            var statusLookup = new StatusLookup();
+            statusLookup.Load();
+            var status = statusLookup.Match(0);
+
+            Assert.That(status, Is.EqualTo("Currently waiting that you do something"));
+        }
+    }
+}
--- Lookum.Framework.Testing/Lookup/EnumLookupTest.cs
+++ Lookum.Framework.Testing/Lookup/EnumLookupTest.cs
@@ -21,28 +21,12 @@ namespace Lookum.Framework.Testing.Lookup
         }
 
         
-        public enum Status : short
-        {
-            [System.ComponentModel.Description("Currently waiting that you do something")]
-            Waiting = 0,
-            [System.ComponentModel.Description("Currently in action")]
-            InAction = 1
-        }
-
         public class CardinalLookup : EnumLookup<Cardinal, int>
         {
             public CardinalLookup()
                 : base("Unknown")
             {
                 
-            }
-        }
-        public class StatusLookup : EnumDescriptionLookup<Status, short>
-        {
-            public StatusLookup()
-                : base("Unknown")
-            {
-
             }
         }
 
@@ -66,14 +50,5 @@ namespace Lookum.Framework.Testing.Lookup
             Assert.That(country, Is.EqualTo("Unknown"));
         }
 
-        [Test]
-        public void Match_AdvancedExistingKey_ReturnValue()
-        {
-            var statusLookup = new StatusLookup();
-            statusLookup.Load();
-            var status = statusLookup.Match(0);
-
-            Assert.That(status, Is.EqualTo("Currently waiting that you do something"));
-        }
     }
 }
