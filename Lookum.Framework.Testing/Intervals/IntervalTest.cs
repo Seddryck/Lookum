b0727ac8f6eea64a7ac3f35d705b272a2172d0cb
Cedric L. Charlier <cedric.charlier@yucom.be>
2014-09-18 19:30:25 +0200

    Creation of the notion of Interval and intersection

---
+++ Lookum.Framework.Testing/Intervals/IntervalTest.cs
@@ -0,0 +1,43 @@
+﻿using Lookum.Framework.Intervals;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lookum.Framework.Testing.Intervals
+{
+    public class IntervalTest
+    {
+        public class MyObject
+        {
+            public DateTime StartLife { get; set; }
+            public DateTime EndLife { get; set; }
+            public DateTime StartSecondLife { get; set; }
+            public DateTime EndSecondLife { get; set; }
+
+        }
+
+        [Test]
+        public void Intersect_TwoIntervals_CorrectResult()
+        {
+            var myObject = new MyObject()
+            {
+                StartLife = new DateTime(2014, 1, 1),
+                EndLife = new DateTime(2015, 1, 1),
+                StartSecondLife = new DateTime(2014, 6, 1),
+                EndSecondLife = new DateTime(2015, 6, 1),
+            };
+
+            var result = Interval<DateTime>.Intersect
+                (
+                    new List<DateTime>() {myObject.StartLife, myObject.StartSecondLife},
+                    new List<DateTime>() {myObject.EndLife, myObject.EndSecondLife}
+                );
+
+            Assert.That(result.Start, Is.EqualTo(new DateTime(2014, 6, 1)));
+            Assert.That(result.End, Is.EqualTo(new DateTime(2015, 1, 1)));
+        }
+    }
+}
---
+++ Lookum.Framework/Intervals/Interval.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lookum.Framework.Intervals
+{
+    public class Interval<T>
+    {
+        public T Start { get; set; }
+        public T End { get; set; }
+
+        public static Interval<T> Intersect(IEnumerable<T> starts, IEnumerable<T> ends)
+        {
+            var minStart = starts.Min();
+            var maxStart = starts.Max();
+            
+            var minEnd = ends.Min();
+            var maxEnd = starts.Max();
+
+            return new Interval<T>() { Start = maxStart, End = minEnd };
+        }
+    }
+}
